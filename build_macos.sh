d05d1dafc058b80970fe37f9305b7a7679b31b14
Joseph Lyncheski <directmusic94@gmail.com>
2022-09-05 11:14:59 -0400

    Switch to RelWithDebInfo for cmake.

--- build_macos.sh
+++ build_macos.sh
@@ -1,5 +1,5 @@
 set -e
-cmake -B build-macos -DCMAKE_OSX_DEPLOYMENT_TARGET=10.13 -DCMAKE_BUILD_TYPE=Release
+cmake -B build-macos -DCMAKE_OSX_DEPLOYMENT_TARGET=10.13 -DCMAKE_BUILD_TYPE=RelWithDebInfo
 cmake --build build-macos --target MiniMetersServer_VST3
 cmake --build build-macos --target MiniMetersServer_AU
 
@@ -7,8 +7,8 @@ mkdir -p ./releases
 #cleanupp
 rm -r -f ./releases/plugin_macos.zip
 #moving
-cp -r ./build-macos/MiniMetersServer_artefacts/Release/VST3/MiniMetersServer.vst3 ./releases
-cp -r ./build-macos/MiniMetersServer_artefacts/Release/AU/MiniMetersServer.component ./releases
+cp -r ./build-macos/MiniMetersServer_artefacts/RelWithDebInfo/VST3/MiniMetersServer.vst3 ./releases
+cp -r ./build-macos/MiniMetersServer_artefacts/RelWithDebInfo/AU/MiniMetersServer.component ./releases
 #zipping
 cd releases
 echo "ZIP"
