b6014a9398e5c20d4e51b8d9fb27475f22d3c935
Joseph Lyncheski <directmusic94@gmail.com>
2022-12-20 14:24:50 -0500

    Linux build script includes CLAP format.

--- build_linux.sh
+++ build_linux.sh
@@ -1,8 +1,9 @@
 set -e
 cmake -B build-linux -DCMAKE_BUILD_TYPE=Release
 cmake --build build-linux --target MiniMetersServer_VST3
+cmake --build build-linux --target MiniMetersServer_CLAP
 #zip
 echo "Zipping to plugin_linux.zip"
 mkdir -p ./releases
 rm -r -f ./releases/plugin-linux.zip
-zip -r ./releases/plugin-linux.zip ./build-linux/MiniMetersServer_artefacts/Release/VST3/MiniMetersServer.vst3
+zip -r ./releases/plugin-linux.zip ./build-linux/MiniMetersServer_artefacts/Release/VST3/MiniMetersServer.vst3 ./build-linux/MiniMetersServer_artefacts/Release/CLAP/MiniMetersServer.clap
