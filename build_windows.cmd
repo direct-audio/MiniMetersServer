2265d388c078810e71e6eb7bfc0fe9a2bdae3d19
Joseph Lyncheski <directmusic94@gmail.com>
2023-01-26 15:17:02 -0500

    Updated Windows build script.

--- build_windows.cmd
+++ build_windows.cmd
@@ -1,6 +1,13 @@
+echo "Building"
+if not exist .\build mkdir .\build
+cd .\build
+cmake ..\
+cmake --build . --config Release
+cd ..
+echo "Copying to zip"
 mkdir .\releases
-Xcopy /E /I .\cmake-build-release\MiniMetersServer_artefacts\Release\VST3\MiniMetersServer.vst3 .\releases\MiniMetersServer.vst3
-copy .\cmake-build-release\MiniMetersServer_artefacts\Release\CLAP\MiniMetersServer.clap .\releases
+Xcopy /E /I .\build\MiniMetersServer_artefacts\Release\VST3\MiniMetersServer.vst3 .\releases\MiniMetersServer.vst3
+copy .\build\MiniMetersServer_artefacts\Release\CLAP\MiniMetersServer.clap .\releases
 cd releases
 tar.exe -a -c -f plugin-windows.zip MiniMetersServer.clap MiniMetersServer.vst3
 cd ..
