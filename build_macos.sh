c1e41364033f894b2e1bdb466d07836c2572b7c7
Joseph Lyncheski <directmusic94@gmail.com>
2023-03-18 20:56:14 -0400

    Update build scripts and add GitHub Actions

--- build_macos.sh
+++ build_macos.sh
@@ -3,16 +3,22 @@ cmake -B build-macos -DCMAKE_OSX_DEPLOYMENT_TARGET=10.13 -DCMAKE_BUILD_TYPE=RelW
 cmake --build build-macos --target MiniMetersServer_VST3
 cmake --build build-macos --target MiniMetersServer_AU
 cmake --build build-macos --target MiniMetersServer_CLAP
-
 mkdir -p ./releases
-#cleanupp
+
+#cleanup
 rm -r -f ./releases/plugin_macos.zip
+
 #moving
 cp -r ./build-macos/MiniMetersServer_artefacts/RelWithDebInfo/VST3/MiniMetersServer.vst3 ./releases
 cp -r ./build-macos/MiniMetersServer_artefacts/RelWithDebInfo/AU/MiniMetersServer.component ./releases
 cp -r ./build-macos/MiniMetersServer_artefacts/RelWithDebInfo/CLAP/MiniMetersServer.clap ./releases
+
 #zipping
 cd releases
 echo "ZIP"
-rm -r -f ./plugin_macos.zip
-zip -r ./plugin_macos.zip ./MiniMetersServer.vst3 ./MiniMetersServer.component ./MiniMetersServer.clap
+zip -r ./plugin-macos.zip ./MiniMetersServer.vst3 ./MiniMetersServer.component ./MiniMetersServer.clap
+
+#cleanup
+rm -r -f ./MiniMetersServer.vst3
+rm -r -f ./MiniMetersServer.component
+rm -r -f ./MiniMetersServer.clap
