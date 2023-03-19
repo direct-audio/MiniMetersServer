4dddf26e04eff47807dd374564dcd059f70b11a1
Joseph Lyncheski <directmusic94@gmail.com>
2023-03-19 15:27:35 -0400

    Quick exit on build failure.

--- build_windows.cmd
+++ build_windows.cmd
@@ -1,15 +1,22 @@
 echo "Building"
+:: make build folder
 if not exist .\build mkdir .\build
 cd .\build
-cmake ..\
-cmake --build . --config Release
+
+:: build
+cmake ..\ || exit /b
+cmake --build . --config Release || exit /b
+
 cd ..
 echo "Copying to zip"
+
 mkdir .\releases
+
 Xcopy /E /I .\build\MiniMetersServer_artefacts\Release\VST3\MiniMetersServer.vst3 .\releases\MiniMetersServer.vst3
 copy .\build\MiniMetersServer_artefacts\Release\CLAP\MiniMetersServer.clap .\releases
 cd releases
 tar.exe -a -c -f plugin-windows.zip MiniMetersServer.clap MiniMetersServer.vst3
 cd ..
+
 if exist .\releases\MiniMetersServer.vst3 rmdir /s /Q .\releases\MiniMetersServer.vst3
 if exist .\releases\MiniMetersServer.clap del .\releases\MiniMetersServer.clap
