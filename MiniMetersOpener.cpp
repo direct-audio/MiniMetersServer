b2dc78c9da3bfdaa2c3e4b2a6d16c5c2016e27e9
Joseph Lyncheski <directmusic94@gmail.com>
2023-03-18 21:13:14 -0400

    Fix windows build.

--- MiniMetersOpener.cpp
+++ MiniMetersOpener.cpp
@@ -1,5 +1,99 @@
-//
-// Created by Joseph Lyncheski on 1/30/23.
-//
-
 #include "MiniMetersOpener.h"
+
+#if defined(__APPLE__)
+#include "MacOsHelpers.h"
+#elif defined(_WIN32)
+#include <windows.h>
+#include <processthreadsapi.h>
+#include <tlhelp32.h>
+#endif
+#include <string>
+#include <fstream>
+
+
+bool MiniMetersOpener::is_minimeters_running() {
+#if defined(__APPLE__)
+    // Naive way of checking on macOS.
+    // bool is_running = system("ps -Ac | grep 'MiniMeters' > /dev/null") == 0;
+    return MacOsHelpers::is_minimeters_running();
+#elif defined(_WIN32)
+    // Pulled from here: https://stackoverflow.com/questions/3477097/get-full-running-process-list-visual-c
+    std::string compare;
+    bool minimeters_running = false;
+
+    HANDLE process_snapshot_handle;
+    PROCESSENTRY32 pe32;
+    process_snapshot_handle = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+    if (process_snapshot_handle == INVALID_HANDLE_VALUE) {
+        minimeters_running = false;
+    } else {
+        pe32.dwSize = sizeof(PROCESSENTRY32);
+        if (Process32First(process_snapshot_handle, &pe32)) { // Gets first running process
+            if (std::string(pe32.szExeFile) == std::string("MiniMeters.exe")) {
+                minimeters_running = true;
+            } else {
+                // loop through all running processes looking for process
+                while (Process32Next(process_snapshot_handle, &pe32)) {
+                    if (std::string(pe32.szExeFile) == "MiniMeters.exe") {
+                        // if found process is running, set to true and break from loop
+                        minimeters_running = true;
+                        break;
+                    }
+                }
+            }
+            // clean the snapshot object
+            CloseHandle(process_snapshot_handle);
+        }
+    }
+
+    return minimeters_running;
+#endif
+    // Assume it is running otherwise.
+    return true;
+}
+
+static bool does_file_exist(const std::string& path) {
+    std::ifstream f(path.c_str());
+    return f.good();
+}
+
+bool MiniMetersOpener::launch_minimeters() {
+#if defined(__APPLE__)
+    MacOsHelpers::open_minimeters();
+#elif defined(_WIN32)
+    // Pulled from here: https://stackoverflow.com/questions/15435994/how-do-i-open-an-exe-from-another-c-exe
+    STARTUPINFOA startup_info;
+    PROCESS_INFORMATION process_info;
+
+    ZeroMemory(&startup_info, sizeof(startup_info));
+    startup_info.cb = sizeof(startup_info);
+    ZeroMemory(&process_info, sizeof(process_info));
+    TCHAR args[] = "MiniMeters.exe -d \"MiniMeters Plugin\"";
+    LPCSTR full_version_path = "C:/Program Files/MiniMeters/MiniMeters.exe";
+    LPCSTR demo_version_path = "C:/Program Files/MiniMeters-demo/MiniMeters-demo.exe";
+    LPCSTR path;
+    if (does_file_exist(full_version_path)) {
+        path = full_version_path;
+    } else if (does_file_exist(demo_version_path)) {
+        path = demo_version_path;
+    } else {
+        return false;
+    }
+    CreateProcessA(
+        path,
+        args,
+        nullptr,
+        nullptr,
+        FALSE,
+        CREATE_NEW_CONSOLE,
+        nullptr,
+        nullptr,
+        &startup_info,
+        &process_info);
+
+    CloseHandle(process_info.hProcess);
+    CloseHandle(process_info.hThread);
+#endif
+    return false;
+}
--- MiniMetersOpener.h
+++ MiniMetersOpener.h
@@ -1,13 +1,6 @@
 #pragma once
-#include "MacOsHelpers.h"
 #include <JuceHeader.h>
 #include <cstdlib>
-#if defined(_WIN32)
-#include <processthreadsapi.h>
-#include <string>
-#include <tlhelp32.h>
-#include <windows.h>
-#endif
 
 class MiniMetersOpener {
 
@@ -17,76 +10,6 @@ public:
         Failure
     };
 
-    static bool is_minimeters_running() {
-#if defined(__APPLE__)
-        // Naive way of checking on macOS.
-        // bool is_running = system("ps -Ac | grep 'MiniMeters' > /dev/null") == 0;
-        return MacOsHelpers::is_minimeters_running();
-#elif defined(_WIN32)
-        // Pulled from here: https://stackoverflow.com/questions/3477097/get-full-running-process-list-visual-c
-        std::string compare;
-        bool minimeters_running = false;
-
-        HANDLE process_snapshot_handle;
-        PROCESSENTRY32 pe32;
-        process_snapshot_handle = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-
-        if (process_snapshot_handle == INVALID_HANDLE_VALUE) {
-            minimeters_running = false;
-        } else {
-            pe32.dwSize = sizeof(PROCESSENTRY32);
-            if (Process32First(process_snapshot_handle, &pe32)) { // Gets first running process
-                if (std::string(pe32.szExeFile) == std::string("MiniMeters.exe")) {
-                    minimeters_running = true;
-                } else {
-                    // loop through all running processes looking for process
-                    while (Process32Next(process_snapshot_handle, &pe32)) {
-                        if (std::string(pe32.szExeFile) == "MiniMeters.exe") {
-                            // if found process is running, set to true and break from loop
-                            minimeters_running = true;
-                            break;
-                        }
-                    }
-                }
-                // clean the snapshot object
-                CloseHandle(process_snapshot_handle);
-            }
-        }
-
-        return minimeters_running;
-#endif
-        // Assume it is running otherwise.
-        return true;
-    }
-
-    static bool launch_minimeters() {
-#if defined(__APPLE__)
-//        juce::Process::openDocument("com.josephlyncheski.minimeters", "-d \"MiniMeters Plugin\"");
-        MacOsHelpers::open_minimeters();
-#elif defined(_WIN32)
-        // Pulled from here: https://stackoverflow.com/questions/15435994/how-do-i-open-an-exe-from-another-c-exe
-        STARTUPINFOA startup_info;
-        PROCESS_INFORMATION process_info;
-
-        ZeroMemory(&startup_info, sizeof(startup_info));
-        startup_info.cb = sizeof(startup_info);
-        ZeroMemory(&process_info, sizeof(process_info));
-
-        CreateProcessA(
-            "C:/Program Files/MiniMeters/MiniMeters.exe",
-            "-d \"MiniMeters Plugin\"",
-            nullptr,
-            nullptr,
-            FALSE,
-            CREATE_NEW_CONSOLE,
-            nullptr,
-            nullptr,
-            &startup_info,
-            &process_info);
-
-        CloseHandle(pi.hProcess);
-        CloseHandle(pi.hThread);
-#endif
-        return false;
-    }
+    static bool is_minimeters_running();
+    static bool launch_minimeters();
 };
