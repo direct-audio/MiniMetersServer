843f65eed2b23c7e311cbfef3718ab9066273133
Joseph Lyncheski <directmusic94@gmail.com>
2023-08-19 17:18:37 -0400

    Cleaup

--- LookAndFeel.h
+++ LookAndFeel.h
@@ -1,5 +1,4 @@
 #pragma once
-#include "BinaryData.h"
 #include <JuceHeader.h>
 
 class MiniMetersLookAndFeel : public juce::LookAndFeel_V4 {
@@ -8,8 +7,8 @@ public:
         minimeters_font = juce::Font(juce::Typeface::createSystemTypefaceFor(BinaryData::SpaceMonoRegular_ttf, BinaryData::SpaceMonoRegular_ttfSize));
     }
     juce::Font minimeters_font;
-    void drawButtonBackground(juce::Graphics& g, juce::Button& button, const juce::Colour& backgroundColour, bool, bool isButtonDown) override {
-        if (isButtonDown) {
+    void drawButtonBackground(juce::Graphics& g, juce::Button& button, const juce::Colour& background_colour, bool, bool is_button_down) override {
+        if (is_button_down) {
             g.setColour({ 172 / 2, 192 / 2, 222 / 2 });
         } else {
             g.setColour({ 172, 192, 222 });
@@ -17,27 +16,27 @@ public:
         g.fillRect(button.getLocalBounds());
     }
 
-    void drawButtonText(juce::Graphics& g, juce::TextButton& button, bool, bool isButtonDown) override {
+    void drawButtonText(juce::Graphics& g, juce::TextButton& button, bool, bool is_button_down) override {
         auto font = getTextButtonFont(button, button.getHeight());
         g.setFont(minimeters_font.withHeight(24));
-        if (isButtonDown) {
+        if (is_button_down) {
             g.setColour({ 255, 255, 255 });
         } else {
             g.setColour({ 0, 0, 0 });
         }
 
-        auto yIndent = juce::jmin(4, button.proportionOfHeight(0.3f));
-        auto cornerSize = juce::jmin(button.getHeight(), button.getWidth()) / 2;
+        auto y_indent = juce::jmin(4, button.proportionOfHeight(0.3f));
+        auto corner_size = juce::jmin(button.getHeight(), button.getWidth()) / 2;
 
-        auto fontHeight = juce::roundToInt(font.getHeight() * 0.6f);
-        auto leftIndent = juce::jmin(fontHeight, 2 + cornerSize / (button.isConnectedOnLeft() ? 4 : 2));
-        auto rightIndent = juce::jmin(fontHeight, 2 + cornerSize / (button.isConnectedOnRight() ? 4 : 2));
-        auto textWidth = button.getWidth() - leftIndent - rightIndent;
+        auto font_height = juce::roundToInt(font.getHeight() * 0.6f);
+        auto left_indent = juce::jmin(font_height, 2 + corner_size / (button.isConnectedOnLeft() ? 4 : 2));
+        auto right_indent = juce::jmin(font_height, 2 + corner_size / (button.isConnectedOnRight() ? 4 : 2));
+        auto text_width = button.getWidth() - left_indent - right_indent;
 
         auto edge = 4;
-        if (textWidth > 0)
+        if (text_width > 0)
             g.drawFittedText(button.getButtonText(),
-                             leftIndent, yIndent, textWidth, button.getHeight() - yIndent * 2 - edge,
+                             left_indent, y_indent, text_width, button.getHeight() - y_indent * 2 - edge,
                              juce::Justification::centred, 2);
     }
 };
